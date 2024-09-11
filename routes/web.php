ce41729b257002ae1b42b247e5e980de0ac01758
andrejprus <goleaf@gmail.com>
2024-09-11 20:39:42 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
+use App\Http\Livewire\Users;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Livewire\Books;
+use App\Http\Livewire\Authors;
+use App\Http\Livewire\Genres;
+
 use App\Http\Livewire\Login;
 use App\Http\Livewire\Register;
 
+
+
 //Route::get('/books', Books::class)->name('books.index')->middleware('auth');
 
+
+/*
 Route::get('/', function () {
     return view('books.index');
 })->name('books.index');//->middleware('auth');
@@ -14,7 +23,12 @@ Route::get('/', function () {
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 
+*/
 
 
 
 
+Route::get('/books', Books::class)->name('books.index')->middleware('auth');
+Route::get('/authors', Authors::class)->name('authors.index')->middleware('auth');
+Route::get('/genres', Genres::class)->name('genres.index')->middleware('auth');
+Route::get('/users', Users::class)->name('users.index')->middleware('auth');
